5facccbf4cb726b990a5df96d1c49a2258e81f37
Andrew Sardone <andrew@andrewsardone.com>
2013-12-15 17:04:17 -0500

    Add empty view Xcode project template

---
+++ RefreshControlMask/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RefreshControlMask
+//
+//  Created by Andrew Sardone on 12/15/13.
+//  Copyright (c) 2013 Andrew Sardone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
